bca05d4c961ea19299f0b767b30c28a43a36673d
michael <mpthornton86@gmail.com>
2020-10-24 12:35:08 -0600

    commit tree 9cef1363888c3c453772a0173c56261473ccbb3d

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+    counter = 0
+    values = []
+    while counter < collection.length
+        values << yield(collection[counter])
+        counter += 1
+    end
+    if values.include?(false)
+        false
+    else
+        true
+    end
 end
